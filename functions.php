f7aba97454bacbd66d885400d4763b6b0695537c
Jacek Jarczok <kson.eu@gmail.com>
2020-02-24 11:20:14 +0100

    Add google fonts

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
   function homesales_files() {
+    wp_enqueue_style('custom-fonts', 'https://fonts.googleapis.com/css?family=Lato|Montserrat:400,500,700&display=swap', false);
     wp_enqueue_style('hamburger_menu_styles', get_theme_file_uri('/css/hamburgers.css'));
     wp_enqueue_style('homesales_main_styles', get_stylesheet_uri());
     wp_enqueue_script('homesales_main_script', get_theme_file_uri('/js/script.js'), NULL, '1.0', true);
