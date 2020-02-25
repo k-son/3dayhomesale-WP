39f79a9855447506ba7d95f299b73abcfb736a73
Jacek Jarczok <kson.eu@gmail.com>
2020-02-25 15:33:49 +0100

    Develop blog and pages

--- single.php
+++ single.php
@@ -2,19 +2,18 @@
   get_header(); 
   ?>
 <div class="wrapper">
-<header class="blog__welcome">
-  <div class="blog__welcome__txt-box">
-    <h1 class="blog__h1"><?php the_title(); ?></h1>
-    <a class="blog__back-to-blog" href="<?php echo site_url('/blog'); ?>">&larr; blog</a>
-  </div>
-</header>
+  <header class="blog__welcome">
+    <div class="blog__welcome__txt-box">
+      <h1 class="blog__h1"><?php the_title(); ?></h1>
+      <a class="blog__back-to-blog" href="<?php echo site_url('/blog'); ?>">&larr; blog</a>
+    </div>
+  </header>
 
-
-<main class="blog__main" style="margin-top: 2rem;">
-<?php 
-  while (have_posts()) {
+  <main class="blog__main" style="margin-top: 2rem;">
+  <?php 
+    while (have_posts()) {
     the_post();
-    ?>
+  ?>
     <div class="blog__content">
       <div class="blog__post__meta" style="margin-bottom: 6rem;">
         <p>Posted by <span class="blog__post__meta__author"><?php the_author_posts_link();?></span> on <span class="blog__post__meta__time"><?php the_time('M j, Y'); ?></span> in <span class="blog__post__meta__category"><?php echo get_the_category_list(', '); ?></span></p>
@@ -24,9 +23,15 @@
       </div>
     </div>
     <?php } ?>
-    <div class="pagination">
-    <?php echo paginate_links(); ?>
+    <div class="blog__more-posts">
+      <div class="blog__prvious-post">
+          <?php previous_post_link('%link', '« %title', false); ?>
+      </div>
+      <div class="blog__next-post">
+          <?php next_post_link('%link', '%title »', false); ?>
+      </div>
     </div>
   </main>
-  </div>
+
+</div>
 <?php get_footer(); ?>
