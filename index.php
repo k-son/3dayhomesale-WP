4c762cd000e1ab81539d865d7c79d0985a831c71
Jacek Jarczok <kson.eu@gmail.com>
2020-02-25 16:09:56 +0100

    Develop

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
       <div class="blog__content">
         <h2 class="blog__post__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <div class="blog__post__meta">
-          <p>Posted by <span class="blog__post__meta__author"><?php the_author_posts_link();?></span> on <span class="blog__post__meta__time"><?php the_time('M j, Y'); ?></span> in <span class="blog__post__meta__category"><?php echo get_the_category_list(', '); ?></span></p>
+          <p>Posted by <span class="blog__post__meta__author"><?php the_author_posts_link();?></span> on <span class="blog__post__meta__time"><?php the_time('M j, Y'); ?></span> in <span class="blog__post__meta__category"><?php echo get_the_category_list(' &middot; '); ?></span></p>
         </div>
         <div class="blog__post__text">
           <?php the_excerpt(); ?>
