70defc4f0baade3970cd0c1e6e23a560a7fdf15d
Jacek Jarczok <kson.eu@gmail.com>
2020-02-28 11:15:48 +0100

    Develop

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@
 
   function homesales_features() {
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
   }
 
   add_action('after_setup_theme', 'homesales_features')
--- single.php
+++ single.php
@@ -18,14 +18,18 @@
       <div class="blog__post__meta" style="margin-bottom: 6rem;">
         <p>Posted by <span class="blog__post__meta__author"><?php the_author_posts_link();?></span> on <span class="blog__post__meta__time"><?php the_time('M j, Y'); ?></span> in <span class="blog__post__meta__category"><?php echo get_the_category_list(', '); ?></span></p>
       </div>
+      <div class="blog__post__featured-image">
+        <?php if (has_post_thumbnail()) the_post_thumbnail(array(600, 400)); ?>
+      </div>
       <div class="blog__post__text">
         <?php the_content(); ?>
       </div>
     </div>
     <?php } ?>
     <div class="blog__more-posts">
-      <div class="blog__prvious-post">
-          <?php previous_post_link('%link', '« %title', false); ?>
+      <div class="blog__previous-post">
+          <?php
+            previous_post_link('%link', '« %title' ,false); ?>
       </div>
       <div class="blog__next-post">
           <?php next_post_link('%link', '%title »', false); ?>
