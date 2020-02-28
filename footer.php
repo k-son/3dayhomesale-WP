5a4a057fab7b40a8b9470a380688389505f7cd82
Jacek Jarczok <kson.eu@gmail.com>
2020-02-28 16:22:21 +0100

    Delete privacy policy link

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
     <footer class="footer">
       <span class="footer__span footer__span--1">Copyright All Right Reserved 2017
       <a href="<?php echo site_url('/index.php/'); ?>" class="footer__span orange-color">3dayhomesale</a></span>
-      <span class="footer__span"><a href="<?php echo site_url('/privacy-policy'); ?>" class="footer__a ">Privacy Policy</a></span>
     </footer>
   </div>
 <?php wp_footer(); ?>
