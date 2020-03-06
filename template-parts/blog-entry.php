84032b40b458f78b0c070b7f7e8e6cf9390f8390
Jacek Jarczok <kson.eu@gmail.com>
2020-03-06 17:03:00 +0100

    Add if statement

--- template-parts/blog-entry.php
+++ template-parts/blog-entry.php
@@ -1,8 +1,10 @@
 <div class="blog__content">
   <h2 class="blog__post__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <?php if (has_category()) { ?>
   <div class="blog__post__meta">
-    <p>Posted by <span class="blog__post__meta__author"><?php the_author_posts_link();?></span> on <span class="blog__post__meta__time"><?php the_time('M j, Y'); ?></span> in <span class="blog__post__meta__category"><?php echo get_the_category_list(', '); ?></span></p>
+      <p>Posted by <span class="blog__post__meta__author"><?php the_author_posts_link(); ?></span> on <span class="blog__post__meta__time"><?php the_time('M j, Y'); ?></span> in <span class="blog__post__meta__category"><?php echo get_the_category_list(', '); ?></span></p>
   </div>
+  <?php } ?>
   <div class="blog__archive__featured-image">
     <a href="<?php the_permalink(); ?>"><?php if (has_post_thumbnail()) the_post_thumbnail(array(300, 300)); ?></a>   
   </div>
