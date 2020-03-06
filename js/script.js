86fd98fda198bdf729317283ce3b9b3315c5031a
Jacek Jarczok <kson.eu@gmail.com>
2020-03-06 14:25:57 +0100

    Develop search bar

--- js/script.js
+++ js/script.js
@@ -13,6 +13,10 @@ const navLinks = document.querySelectorAll('.nav__list__link');
 const hamburger = document.querySelector('.hamburger');
 const hamburgerInner = document.querySelector('.hamburger-inner');
 
+const searchIcon = document.querySelector('.search__icon');
+const searchContainer = document.querySelector('.search__container');
+const searchInput = document.querySelector('#s');
+
 function whiteNavbar() {
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     navbar.classList.add('whiteNavbar');
@@ -20,15 +24,19 @@ function whiteNavbar() {
     //navbar.style.backgroundColor = "#fff";
     navLinks.forEach(el => el.classList.add('whiteNavLinkText'));
     hamburgerInner.style.backgroundColor = hamburgerGrey;
+    searchIcon.classList.add('search__icon--whiteNavbar');
 
   } else {
     navbar.classList.remove('whiteNavbar');
     nav.style.paddingTop = "30px";
     //navbar.style.backgroundColor = "#121d26";
     hamburgerInner.style.backgroundColor = colorWhite;
+    searchIcon.classList.remove('search__icon--whiteNavbar');
   }
 }
 
+
+// Hamburger menu
 function toggleHamburger() {
   hamburger.classList.toggle('is-active');
   navList.classList.toggle('displayFlex');
@@ -43,3 +51,11 @@ navLinks.forEach(el => {
 })
 
 
+// Searching
+function toggleSearchBar() {
+  searchContainer.classList.toggle('display-none');
+}
+
+searchIcon.addEventListener('click', toggleSearchBar);
+
+searchInput.placeholder = "Type in...";
