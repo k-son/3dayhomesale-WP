3f747814722bfc31ea2862a5c4c7ffe03457b92e
Jacek Jarczok <kson.eu@gmail.com>
2020-03-19 16:24:36 +0100

    Create 404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php 
+  get_header(); 
+  ?>
+<div class="wrapper">
+  <header class="blog__welcome">
+  </header>
+  
+    <div class="page404__box blog__content page__content">
+      <p class="page404__oops">oops! nothing was found</p>
+      <p class="page404__404">404</p>
+      <p class="page404__sorry">we are sorry, but the page you requested was not found</p>
+      <p class="page404__back-to-main"><a href="<?php echo site_url('/'); ?>" class="page404__link">&larr; go back to main page</a></p>
+    </div>
+
+</div>
+
