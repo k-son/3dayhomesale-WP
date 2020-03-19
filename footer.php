01224c81f38442aa7208803d5fbd63154195bc5d
Jacek Jarczok <kson.eu@gmail.com>
2020-03-19 15:53:25 +0100

    Insert date method

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer class="footer">
-      <span class="footer__span footer__span--1">Copyright All Right Reserved 2017
+      <span class="footer__span footer__span--1">Copyright All Right Reserved <?php echo date('Y'); ?>
       <a href="<?php echo site_url('/index.php/'); ?>" class="footer__span orange-color">3dayhomesale</a></span>
     </footer>
   </div>
